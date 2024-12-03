5cc028a54bbbfcc3b2c9a3b8640fa8b1c64bcc56
Fany Quivano <faquiva110@gmail.com>
2024-12-03 01:13:31 -0500

    creacion proyecto

---
+++ src/main/java/com/aluracursos/LiterAlura_ClallengeJava/LiterAluraClallengeJavaApplication.java
@@ -0,0 +1,17 @@
+package com.aluracursos.LiterAlura_ClallengeJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraClallengeJavaApplication {
+
+	public static void main(String[] args) {
+
+		//SpringApplication.run(LiterAluraClallengeJavaApplication.class, args);
+		System.out.println("""
+				
+				""");
+	}
+
+}
---
+++ src/test/java/com/aluracursos/LiterAlura_ClallengeJava/LiterAluraClallengeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.LiterAlura_ClallengeJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraClallengeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
