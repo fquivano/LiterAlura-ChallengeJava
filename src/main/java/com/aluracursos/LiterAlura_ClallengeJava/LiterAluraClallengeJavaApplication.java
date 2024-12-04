e2bf3ea0be81ed336a058c663a0d2c3cd93aaad4
Fany Quivano <faquiva110@gmail.com>
2024-12-03 22:03:23 -0500

    se implementa el menu del aplicativo

--- src/main/java/com/aluracursos/LiterAlura_ClallengeJava/LiterAluraClallengeJavaApplication.java
+++ src/main/java/com/aluracursos/LiterAlura_ClallengeJava/LiterAluraClallengeJavaApplication.java
@@ -10,7 +10,13 @@ public class LiterAluraClallengeJavaApplication {
 
 		//SpringApplication.run(LiterAluraClallengeJavaApplication.class, args);
 		System.out.println("""
-				
+				Elija la opcion a traves de su numero:
+				1 - Buscar libro por titulo
+				2 - Listar libros registrados
+				3 - Listar autores registrados
+				4 - Listar autores vivos en un determinado año
+				5 - Listar libros por idioma
+				0 - Salir
 				""");
 	}
 
